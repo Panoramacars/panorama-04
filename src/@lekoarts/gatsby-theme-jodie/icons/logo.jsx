b881d588ad8d2c16d9bc513d2c76d9e4292de065
Panoramacars <115028319+Panoramacars@users.noreply.github.com>
2022-10-14 23:05:17 +0400

    Update logo.jsx

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -1,9 +1,7 @@
 import * as React from "react"
 import logo from './Panorama_logo_png_150x150.png';
 <title>Panorama Cars</title>
- # react
 <img src={logo} className="App-logo" alt="logo">
-# react
 .App-logo{
     width: 150px;
 }
